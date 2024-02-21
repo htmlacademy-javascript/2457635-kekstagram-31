12431edf9a2a994a8f912ef31f16697c1bca4d02
Tatiana <balan.tv.20.05@gmail.com>
2024-02-21 17:01:50 +0300

    Нужно больше функций (работа над ошибками)

--- js/function.js
+++ js/function.js
@@ -1,8 +1,44 @@
 // eslint-disable-next-line no-unused-vars
-function checLength (string, maxLength) {
+//задание 1
+// eslint-disable-next-line no-unused-vars
+function checkLength (string, maxLength) {
   if(string.length <= maxLength) {
-    return true;
+  // eslint-disable-next-line indent
+return true;
   } else {
-    return false;
+  // eslint-disable-next-line indent
+return false;
   }
 }
+
+// eslint-disable-next-line no-console
+console.log(checkLength('проверяемая строка',20));
+// eslint-disable-next-line no-console
+console.log(checkLength('проверяемая строка',18));
+// eslint-disable-next-line no-undef, no-console
+console.log(checkLength('проверяемая строка',10));
+
+// Задание 2
+// eslint-disable-next-line no-unused-vars
+const isPalindrom = function (string) {
+  // eslint-disable-next-line prefer-const
+  string = (string.replaceAll(' ','')).toLowerCase();
+  // eslint-disable-next-line prefer-const
+  let testString = '';
+
+  // eslint-disable-next-line no-undef
+  for (i = string.length - 1; i >= 0; i--) {
+    // eslint-disable-next-line no-undef
+    testString += string[i];
+  }
+  return testString === string;
+  // eslint-disable-next-line indent
+  // eslint-disable-next-line semi
+}
+
+// eslint-disable-next-line no-console
+console.log(isPalindrom('топот'));
+// eslint-disable-next-line no-console
+console.log(isPalindrom('ДовОд'));
+// eslint-disable-next-line no-console
+console.log(isPalindrom('Кекс'));
