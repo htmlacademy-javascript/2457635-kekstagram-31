b9fcda8131ef069fdf82fec30f03496c5da6c8ce
Tatiana <balan.tv.20.05@gmail.com>
2024-04-18 20:57:52 +0300

    module9-task1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 import { arrayPhotos } from './data.js';
 import {renderThumbnails} from './thumbnails.js';
 import './big-picture.js';
-import './upload-photo-form.js';
+import {initUploadModal} from './upload-photo-form.js';
 
 renderThumbnails(arrayPhotos);
+initUploadModal();
 
