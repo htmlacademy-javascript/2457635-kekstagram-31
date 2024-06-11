69e626772d388fd0aec0fb89f21adc033d20de33
Tatianos7 <urijurij@MacBook-Air-Urij.local>
2024-06-11 06:25:53 +0300

    сортировка фото

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -4,15 +4,17 @@ const thumbnailsList = document.querySelector('.pictures');
 const thumbnailTemplate = document.querySelector('#picture').content.querySelector('a');
 const thumbnailsFragment = document.createDocumentFragment();
 
+
 function renderThumbnails (arrayPhotos) {
+  resetPhotos();
+
   arrayPhotos.forEach(({ id, url, description, likes, comments }) => {
     const thumbnail = thumbnailTemplate.cloneNode(true);
     const thumbnailsImage = thumbnail.querySelector('.picture__img');
 
     thumbnail.dataset.id = id;
     thumbnailsImage.src = url;
-    thumbnailsImage.alt = description;
-
+    thumbnailsImage.alt = description;renderThumbnails
     thumbnail.querySelector('.picture__likes').textContent = likes;
     thumbnail.querySelector('.picture__comments').textContent = comments.length;
     thumbnailsFragment.append(thumbnail);
@@ -26,16 +28,6 @@ const resetPhotos = () => {
   });
 };
 
-const renderPictures = (data) => {
-  resetPhotos();
-  data.forEach((cardObj) => {
-    fragment.appendChild(fillCardTemplate(cardObj));
-  });
-  document.appendChild(fragment);
-};
-
-const renderPicturesWithDebounce = debounce(renderPictures);
+const renderPicturesWithDebounce = debounce(renderThumbnails);
 
 export { renderThumbnails, renderPicturesWithDebounce };
-
-
